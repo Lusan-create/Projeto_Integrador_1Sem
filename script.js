954898037174e28538d7f3d7442520130d4de247
Cap <spluizguilherme@gmail.com>
2025-05-09 20:39:03 -0300

    Adicionei a parte perdida código css do saida_pecas.html

---
+++ script.js
@@ -0,0 +1,6 @@
+/*----------------------------------- Funções --------------------------------*/
+
+
+
+/*----------------------------- página Index.html ----------------------------*/
+    document.getElementById('bt_login').addEventListener('click',()=>alert('botão clicado'))    
