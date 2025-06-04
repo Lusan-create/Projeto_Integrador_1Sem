9802c307ee194402069e7b592ccd356a383c2fed
Cap <spluizguilherme@gmail.com>
2025-06-04 20:40:55 -0300

    Resolvi a parte do login e estou fazendo o cadastro

--- script.js
+++ script.js
@@ -1,12 +1,63 @@
 /*----------------------------------- Temp --------------------------------*/
+function inicio_cadastro(){
+
+    if (pecas[0].length == 0){
+        pecas = localStorage.getItem("cadastro")
+    }
+        
+};
+
+function cadastro_peca(){
+    let num = pecas.codigo.length + 1;
+
+    /* Código */
+    let codigo = String(num).padStart( 3 , "0" );
+    pecas.codigo.push(codigo);
 
 
+    /* Marca */
+    let marca = document.getElementById("catalogo_marca").value
+    pecas.marca.push(marca);
+
+    /* Nome do produto */
+    let nome = document.getElementById("catalogo_nome").value
+    pecas.nome.push(nome);
+
+    /* Categoria */
+    let categoria = document.getElementById("catalogo_categoria").value
+    pecas.categoria.push(categoria);
+
+    /* Quantidade por embalagem */
+    let quantidade = document.getElementById("catalogo_quantidade").value
+    pecas.quantidade.push(quantidade);
+
+    /* Preco */
+    let preco = document.getElementById("catalogo_preco").value
+    pecas.preco.push(preco);
+
+    /* histórico */
+    let historico = []
+    pecas.historico.push(historico);
+
+    alert(pecas.codigo, pecas.nome, pecas.marca, pecas.categoria, pecas.quantidade, pecas.preco, pecas.historico)
+}
+
+var pecas = {
+    codigo : [],
+    nome : [],
+    marca : [],
+    categoria : [],
+    quantidade : [],
+    preco : [],
+    historico : []
+}
 
 
 
 
 /*----------------------------- página Index.html ----------------------------*/
 var user
+
 
 function inicio(){
     user = 'Visitante'
